3ecb84e1e567c511f9839d6513e87edd3859ab29
Mateen <zahidx297@yahoo.com>
2023-04-29 18:39:28 -0400

    add sorting and duplicate removal to array before building tree

--- tree.js
+++ tree.js
@@ -9,11 +9,17 @@ class Node {
 class Tree {
     constructor(array) {
         this.root = new Node();
+
+        console.log(array); // DEBUG
+        this.sort(array);
+        console.log(array); // DEBUG
+        this.removeDuplicates(array);
+        console.log(array); // DEBUG
+
         this.buildTree(array, this.root);
     }
 
     buildTree(array, root) {
-
         if (!array.length) return;
 
         let mid = Math.trunc(array.length / 2);
@@ -21,10 +27,6 @@ class Tree {
 
         let left = array.slice(0, mid);
         let right = array.slice(mid + 1, array.length);
-
-        console.log('left: ', left);
-        console.log('right: ', right);
-
         if (left.length) {
             root.left = new Node();
             this.buildTree(left, root.left);
@@ -33,17 +35,29 @@ class Tree {
             root.right = new Node();
             this.buildTree(right, root.right);
         }
+    }
 
-
-
-
-        // 0 to (mid - 1), (mid + 1) - (array.length-1)
-
-        // this.root = new Node(array[0]);
-        // this.root.right = new Node(array[1]);
-        // this.root.left = new Node(array[2]);
-        // this.root.right.right = new Node(array[3]);
-        // this.root.left.right = new Node(array[4]);
+    sort(array) {
+        for (let i = array.length - 1; i > 1; i--) {
+            for (let j = 0; j < i; j++) {
+                let first = array[j];
+                let second = array[j + 1];
+                if (second < first) {
+                    array[j + 1] = first;
+                    array[j] = second;
+                }
+            }
+        }
+    }
+    removeDuplicates(array) {
+        for (let i = 0; i < array.length - 1;) {
+            let first = array[i];
+            let second = array[i + 1];
+            if (second === first)
+                array.splice(i,1);
+            else
+                i++;
+        }
     }
 }
 
@@ -60,7 +74,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     }
 }
 
-let array = [1,2,3,4,5,6,7,8,9];
+let array = [1,7,4,23,8,9,4,3,5,5,5,7,9,67,6345,324];
 let tree = new Tree(array);
 console.log(tree);
 prettyPrint(tree.root);
